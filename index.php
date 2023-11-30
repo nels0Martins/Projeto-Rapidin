33e053d19cbb8734cb234ea8b5d39ca0b9ab5a6d
Victor Souza <108535393+Donckz@users.noreply.github.com>
2023-11-30 20:57:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>RAPIDIN</title>
+
+        <!-- ESTILOS E FAVICON -->
+        <link rel="stylesheet" href="styles/index.css">
+        <link rel="shortcut icon" href="assets/logo-a.png" type="image/x-icon">
+        <!------------------>
+    </head>
+    <body>
+
+         <!-- CABEÇALHO COM LOGO, MENU E BOTÕES -->
+        <header class="nave">
+           
+            <!-- MENU -->
+            <nav>
+                <ul>
+                    <li><a href="index.php">INÍCIO</a></li>
+                    <li><a href="#SOBRE">SOBRE</a></li>
+                    <li><a href="#BENEFICIOS">BENEFÍCIOS</a></li>
+                    <li><a href="#BAIXAR">BAIXAR</a></li>
+                </ul>
+            </nav>
+            
+            <!-- LOGO -->
+            <section class="logo">
+                <a href="#"><img src="assets/logo_rapidin.png" alt="Logo do Rapidin"></a>
+            </section>
+            
+            <!-- BOTOES -->
+            <section class="botoes">
+                
+                <article class="box_login">
+                    <a href="entrar.php"><button class="login">LOGIN</button></a>
+                </article>
+                
+                <article class="box_cadastro">
+                    <a href="criarConta.php"><button class="cadastro">CADASTRO</button></a>
+                </article>
+            
+            </section>
+        
+        </header>
+
+        <!-- BOAS VINDAS, APRESENTAÇÃO -->
+        <div class="home">
+            <section class="box_home">
+                <h2>Deseja oferecer uma carona ou pedir uma carona a pessoas que estão indo ao mesmo lugar que você? O Rapidin é a solução!</h2>
+                <p>Conecte-se, economize e compartilhe. Nosso aplicativo de carona facilita sua jornada!</p>
+                <div class="botoes2">
+                    <div class="botao_one">
+                        <a><button>DESEJO OFERECER UMA CARONA</button></a>
+                    </div>
+                    <div class="botao_two">
+                        <a><button>DESEJO UMA CARONA</button></a>
+                    </div>
+                </div>
+            </section>
+        </div>
+   
+        <!-- SOBRE O RAPIDIN -->
+        <section class="sobre" id="SOBRE">
+
+            <section class="box_quatro_img">
+
+                <section class="box_sobre_quatro">
+
+                    <article class="sobre_quatro">
+                        <h2>
+                            Com o aplicativo Rapidin, você terá tudo isso ao alcance de suas mãos. Experimente a facilidade de organizar caronas diárias, conheça pessoas incríveis e desfrute de viagens casuais com conforto e segurança. Liberte-se do transporte público e embarque em uma nova jornada com o Rapidin, o seu companheiro de caronas perfeito.
+                        </h2>
+                    </article>
+
+                </section>
+                
+            </section>
+
+            <section class="box_sobre_um">
+
+                <article class="sobre_um">
+                    <h2>O Projeto Rapidin é a solução perfeita para situações de emergência nas estradas. Seu carro quebrou no meio do caminho? Não se preocupe! Nós estamos aqui para ajudar.
+                    </h2>
+                </article>
+
+                <article class="sobre_um_logo">
+                    <img src="assets/detetive.svg" alt="Procurando por motorista">
+                </article>
+
+            </section>
+
+            <section class="box_sobre_dois">
+
+                <article class="sobre_dois_logo">
+                    <img src="assets/suporte.svg" alt="O suporte do Rapidin é nota 10!">
+                </article>
+
+                <article class="sobre_dois">
+                    <h2>
+                        Suporte rápido e confiável, quando seu carro quebra no meio da estrada, cada minuto conta. Com o Rapidin, você pode contar com a prontidão e a disponibilidade dos nossos motoristas solidários, que estão prontos para ajudar e garantir que você chegue ao seu destino final de forma segura e tranquila.
+                    </h2>
+                </article>
+
+            </section>
+
+            <section class="box_sobre_tres">
+
+                <section class="sobre_tres">
+
+                    <h2>
+                        Por que escolher o Rapidin? Nós nos preocupamos com a sua segurança e bem-estar. Nossa comunidade de motoristas passa por rigorosos processos de verificação e é composta por pessoas confiáveis e solidárias. Além disso, ao utilizar o Rapidin, você estará contribuindo para a redução do trânsito e a preservação do meio ambiente, promovendo a mobilidade sustentável.
+                    </h2>
+
+                </section>
+
+            </section>
+
+        </section>
+
+        <!-- BENEFICIOS DO RAPIDIN -->
+        <section class="beneficios" id="BENEFICIOS">
+
+            <section class="box_um_maxter">
+
+                <article class="box_um_bene">
+                    <img src="assets/comunidade.png" alt="Comunidade">
+                    <h1>Comunidade Solidária</h1>
+                    <p>Ao se juntar ao Rapidin, você faz parte de uma comunidade de pessoas engajadas em ajudar umas às outras. Nossos motoristas são pessoas confiáveis e solidárias, prontas para estender a mão e oferecer suporte em momentos difíceis. Você pode confiar na generosidade e na colaboração dos membros da nossa comunidade.
+                    </p>
+                </article>
+
+                <article class="box_dois_bene">
+                    <img src="assets/seguranca.png" alt="Segurança">
+                    <h1>Segurança em 1°</h1>
+                    <p>Sua segurança é nossa prioridade. Todos os motoristas do Rapidin passam por rigorosos processos de verificação e são avaliados pela comunidade. Além disso, nosso sistema de avaliação permite que os usuários compartilhem suas experiências e garantam a qualidade do serviço.
+                    </p>
+                </article>
+
+                <article class="box_tres_bene">
+                    <img src="assets/energia-sustentavel (1).png" alt="Energia Sustentavel">
+                    <h1>Mobilidade Sustentável</h1>
+                    <p>Ao utilizar o Rapidin, você contribui para a redução do trânsito e a preservação do meio ambiente. Compartilhar caronas significa menos carros nas estradas, o que resulta em menor congestionamento e menor emissão de poluentes. Juntos, podemos fazer a diferença e promover a mobilidade sustentável.
+                    </p>
+                </article>
+
+            </section>
+
+            <div style="display: flex; justify-content: center;">
+                <section class="box_quatro_bene">
+                    <img src="assets/pratico.png" alt="Rápido e prático">
+                    <h1>Experiência Prática e Intuitiva</h1>
+                    <p>Nossa plataforma foi projetada para ser fácil de usar e oferecer uma experiência intuitiva. O processo de solicitar uma carona é simples e rápido. Você poderá acompanhar a localização do motorista em tempo real e se comunicar facilmente com ele. Tudo isso para garantir que você tenha uma experiência prática e conveniente.
+                    </p>
+                </section>
+            </div>
+
+            <!-- BAIXE O APLICATIVO (LOJAS, LINKS SIMULADOS)-->
+            <section class="box_baixar" id="BAIXAR">
+               
+                <div class="baixar-one-center">
+                    <article class="box_baixar_one">
+                        <img src="assets/dois-celulares__1_-removebg-preview.png" alt="Dois celulares com o Rapidin">
+                    </article>
+                    <article class="box_baixar_two">
+                        <h1>Se cuida, vai de Rapidin.</h1>
+                        <h2>Baixe o app e viaje de maneira rápida, simples e segura.</h2>
+                        <div class="app">
+                            <img src="assets/playstore.png" alt="" class="plstore">
+                            <img src="assets/appstore.png" alt="" class="apstore">
+                        </div>
+                        <p>Seja um motorista parceiro Rapidin e aumente seus ganhos.</p>
+                        <div class="buton">
+                            <a href="criarConta.php"><button class="button_baixar">CADASTRE-SE</button></a>
+                        </div>
+                    </article>
+                </div>
+
+            </section>
+
+            <div style="background-image: linear-gradient(to left, #3f7547, #50965b, #61b96e, #6DD37D)">
+                <p style="color: #6DD37D; line-height: 10px;">.</p>
+            </div>
+
+            <!-- RODAPE -->
+            <div class="pezin">
+
+                <footer class="pe">
+
+                    <img src="assets/logo-a.png" alt="Logo do Rapidin">
+                    <ul>
+                        <li><a href="index.php">INÍCIO</a></li>
+                        <li><a href="#SOBRE">SOBRE</a></li>
+                        <li><a href="#BENEFICIOS">BENEFÍCIOS</a></li>
+                        <li><a href="#BAIXAR">BAIXAR</a></li>
+                    </ul>
+
+                    <section class="direitos">
+                        <p style="color: white;">&copy; 2023 Copyright Todos os direitos reservados <a href="usuarios.php"><span style="color: #6DD37D;">Rapidin</span></a>.</p>
+                        <br>
+                    </section>
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
